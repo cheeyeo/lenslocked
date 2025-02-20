f0d20509839ce941cfd596d5ced34c155fb643cf
Chee Yeo <ckyeo.1@gmail.com>
2025-02-20 16:01:13 +0000

    Add router struct

--- main.go
+++ main.go
@@ -15,9 +15,23 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch email me at <a href=\"mailto:test@example.com\">test@example.com</a>")
 }
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server...")
-	http.ListenAndServe(":3000", nil)
+	router := Router{}
+	http.ListenAndServe(":3000", router)
 }
