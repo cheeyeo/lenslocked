ed5f7048fb9825a994f137420fc8a77d5a3427c1
Chee Yeo <ckyeo.1@gmail.com>
2025-02-20 16:18:07 +0000

    Add FAQ and use router struct

--- README.md
+++ README.md
@@ -21,3 +21,24 @@ modd --version
 modd
 ```
 
+* `http.HandlerFunc` is itself a type of func:
+```
+type HandlerFunc func(ResponseWriter, *Request)
+```
+
+its able to declare a ServeHTTP function where it calls itself:
+```
+func (f HandlerFunc) ServeHTTP(w ResponseWriter, r *Request) {
+    f(w, r)
+}
+```
+
+so we can use our own `pathHandler` function and pass it into http.ListenAndServe() via `http.HandleFunc(pathHandler))
+
+In summary:
+1. The HandlerFunc type implements the Handler interface
+
+2. We can convert our handler functions into HandlerFuncs
+
+3. When a function is converted into a HandlerFunc, it has a ServeHTTP method which implements the Handler interface.
+
--- main.go
+++ main.go
@@ -12,7 +12,30 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch email me at <a href=\"mailto:test@example.com\">test@example.com</a>")
+	fmt.Fprintf(w, `<h1>Contact Page</h1>
+<p>To get in touch email me at <a href="mailto:test@example.com">test@example.com</a>`)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<h1>FAQ PAGE</h1>
+<ul>
+	<li>
+		<b>Is there a free version?</b>
+		Yes! We offer a free trial for 30 days on any paid plans.
+	</li>
+	<li>
+		<b>What are your support hours?</b>
+		We have support staff answering emails 24/7, though response
+		times may be a bit slower on weekends.
+	</li>
+	<li>
+		<b>How do I contact support?</b>
+		Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+	</li>
+</ul>
+	
+`)
 }
 
 type Router struct{}
@@ -23,15 +46,15 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server...")
-	router := Router{}
+	var router Router
 	http.ListenAndServe(":3000", router)
 }
